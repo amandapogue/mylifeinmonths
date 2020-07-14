9a87e9accdafa94de9f07328ad27ed427c544cc4
Amanda Pogue <amanda.pogue@sparkbeyond.com>
2020-07-14 00:06:56 -0400

    messing everything up

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
 My life in months, inspired by [Isabella
-Benabaye](https://isabella-b.com/blog/my-life-in-months/)\!
+Benabaye](https://isabella-b.com/blog/my-life-in-months/) and forked from [Sharla Gelfand](https://github.com/sharlagelfand/mylifeinmonths)! 
 
 <img src="life_in_months.png" width="7500" />
--- my_life_in_months.R
+++ my_life_in_months.R
@@ -7,19 +7,20 @@ library(tibble)
 library(forcats)
 library(ggplot2)
 library(tidyr)
+font_
 loadfonts(device = "pdf", quiet = TRUE)
 
 # Create the data-----
 life_data <- expand_grid(
   month = month.name,
-  year = 1991:2020
+  year = 1987:2020
 ) %>%
   mutate(month = fct_relevel(month, month.name)) %>%
   arrange(year, month) %>%
   group_by(year) %>%
   mutate(month_number = row_number()) %>%
   ungroup() %>%
-  filter(!(year == 1991 & month_number < 6))
+  filter(!(year == 1987 & month_number < 11))
 
 # Add "eras" to be coloured ----
 life_data <- life_data %>%
@@ -27,14 +28,14 @@ life_data <- life_data %>%
   mutate(year_month = paste0(c(year, month_number), collapse = ",")) %>%
   ungroup() %>%
   mutate(era = case_when(
-    year_month == "1991,6" ~ "Childhood",
-    year_month == "2006,9" ~ "High School",
-    year_month == "2009,9" ~ "Undergraduate",
-    year_month == "2013,9" ~ "Grad School",
-    year_month == "2015,9" ~ "Unbounce",
-    year_month == "2017,4" ~ "Wattpad",
-    year_month == "2018,7" ~ "College of Nurses of Ontario",
-    year_month == "2019,9" ~ "Freelance R and Shiny Developer"
+    year_month == "1987,11" ~ "Childhood",
+    year_month == "2001,9" ~ "High School",
+    year_month == "2005,9" ~ "Undergraduate",
+    year_month == "2009,9" ~ "Working at NYU",
+    year_month == "2010,9" ~ "MA at Waterloo",
+    year_month == "2013,9" ~ "PhD at Rochester",
+    year_month == "2018,6" ~ "Insight",
+    year_month == "2018,9" ~ "SparkBeyond"
   )) %>%
   fill(era) %>%
   mutate(era = fct_inorder(era))
@@ -86,29 +87,29 @@ life_in_months_role_annotations <- life_in_months_initial_annotations +
   annotate("text", x = 8.5, y = role_annotations_y, label = "childhood", family = role_annotations_font_family, size = roles_size, colour = "#f88f88") +
   annotate("text", x = 17, y = role_annotations_y, label = "highschool", family = role_annotations_font_family, size = roles_size, colour = "#92c9f0") +
   annotate("text", x = 19, y = role_annotations_y - 1.25, label = "undergrad", family = role_annotations_font_family, size = roles_size, colour = "#6eb4e9") +
-  annotate("text", x = 19, y = role_annotations_y - 2.25, label = "(stats)", family = role_annotations_font_family, size = roles_size * 0.75, colour = "#6eb4e9") +
+  annotate("text", x = 19, y = role_annotations_y - 2.25, label = "(pysch)", family = role_annotations_font_family, size = roles_size * 0.75, colour = "#6eb4e9") +
   geom_curve(aes(x = 21.5, xend = 22, y = -1.5, yend = 0.35), curvature = 0.4, arrow = arrow(length = unit(0.0175, "npc")), colour = "#6eb4e9") +
   annotate("text", x = 24.25, y = role_annotations_y, label = "masters", family = role_annotations_font_family, size = roles_size, colour = "#4ca3e4") +
-  annotate("text", x = 24.25, y = role_annotations_y - 1, label = "(also stats)", family = role_annotations_font_family, size = roles_size * 0.75, colour = "#4ca3e4") +
-  annotate("text", x = 27.5, y = role_annotations_y - 1.5, label = "data\nanalyst", family = role_annotations_font_family, lineheight = annotation_lineheight - 0.25, size = roles_size, colour = "#4fc58d") +
+  annotate("text", x = 24.25, y = role_annotations_y - 1, label = "(also psych)", family = role_annotations_font_family, size = roles_size * 0.75, colour = "#4ca3e4") +
+  annotate("text", x = 27.5, y = role_annotations_y - 1.5, label = "PhD\nCogSci", family = role_annotations_font_family, lineheight = annotation_lineheight - 0.25, size = roles_size, colour = "#4fc58d") +
   geom_curve(aes(x = 26.5, xend = 26, y = -1.15, yend = 0.35), curvature = -0.2, arrow = arrow(length = unit(0.0175, "npc")), colour = "#4fc58d") +
   geom_curve(aes(x = 27.5, xend = 28, y = -1, yend = 0.35), curvature = 0.3, arrow = arrow(length = unit(0.0175, "npc")), colour = "#4fc58d") +
-  annotate("text", x = 31, y = role_annotations_y - 0.25, label = "statistician", family = role_annotations_font_family, lineheight = annotation_lineheight, size = roles_size, colour = "#00a87d") +
+  annotate("text", x = 31, y = role_annotations_y - 0.25, label = "Insight", family = role_annotations_font_family, lineheight = annotation_lineheight, size = roles_size, colour = "#00a87d") +
   geom_curve(aes(x = 28.75, xend = 29, y = role_annotations_y, yend = 0.35), curvature = -0.15, arrow = arrow(length = unit(0.0175, "npc")), colour = "#00a87d") +
-  annotate("text", x = 33, y = 1.5, label = "freelance\nR dev", family = role_annotations_font_family, lineheight = annotation_lineheight - 0.25, size = roles_size, colour = "#ab97f3") +
+  annotate("text", x = 33, y = 1.5, label = "Data Scientist\nProduct Lead", family = role_annotations_font_family, lineheight = annotation_lineheight - 0.25, size = roles_size, colour = "#ab97f3") +
   geom_curve(aes(x = 32.75, xend = 30, y = 0.5, yend = 0.35), curvature = -0.3, arrow = arrow(length = unit(0.0175, "npc")), colour = "#ab97f3")
 
 location_colour <- "#8c8c8c"
 
 life_in_months_final <- life_in_months_role_annotations +
-  annotate("text", x = 11, y = 13.1, label = "born + raised in calgary", family = role_annotations_font_family, size = annotation_base_size, colour = location_colour) +
+  annotate("text", x = 11, y = 13.1, label = "born + raised in ~toronto", family = role_annotations_font_family, size = annotation_base_size, colour = location_colour) +
   geom_segment(aes(x = 1, xend = 7, y = 13, yend = 13), colour = location_colour) +
   geom_segment(aes(x = 15, xend = 22, y = 13, yend = 13), colour = location_colour) +
   geom_segment(aes(x = 1, xend = 1, y = 12.75, yend = 13.25), colour = location_colour) +
   geom_segment(aes(x = 22, xend = 22, y = 12.75, yend = 13.25), colour = location_colour) +
-  annotate("text", x = 21, y = 14, label = "moved to vancouver", family = role_annotations_font_family, size = annotation_base_size, lineheight = annotation_lineheight, hjust = 0.75, colour = location_colour) +
+  annotate("text", x = 21, y = 14, label = "moved to nyc", family = role_annotations_font_family, size = annotation_base_size, lineheight = annotation_lineheight, hjust = 0.75, colour = location_colour) +
   geom_curve(aes(x = 22.7, xend = 23, y = 13.8, yend = 12.6), curvature = -0.5, arrow = arrow(length = unit(0.0175, "npc")), colour = location_colour) +
-  annotate("text", x = 27, y = 14, label = "moved to toronto", family = role_annotations_font_family, size = annotation_base_size, lineheight = annotation_lineheight, colour = location_colour) +
+  annotate("text", x = 27, y = 14, label = "moved to waterloo", family = role_annotations_font_family, size = annotation_base_size, lineheight = annotation_lineheight, colour = location_colour) +
   geom_curve(aes(x = 27, xend = 26, y = 13.6, yend = 12.6), curvature = -0.2, arrow = arrow(length = unit(0.0175, "npc")), colour = location_colour)
 
 ggsave("life_in_months.png", plot = life_in_months_final, device = "png", type = "cairo", width = 25, height = 15, dpi = 300)
